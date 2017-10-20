b1bf2e544c7176e18b650494b99fc1ea6c8a3ba0
github3823 <gg47549390@gmail.com>
2017-10-20 20:12:58 +0800

    第7次上传

--- MyBBS/bbs/urls.py
+++ MyBBS/bbs/urls.py
@@ -19,6 +19,8 @@ from bbs import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^category/(\d+)/$', views.category),
+    url(r'^detail/(\d+)/$', views.article_detail,name='article_detail'),
+    url(r'^comment/$', views.comment,name='post_comment'),
 
 
 ]
--- MyBBS/bbs/views.py
+++ MyBBS/bbs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponseRedirect
+from django.shortcuts import render,HttpResponseRedirect,HttpResponse
 from bbs import models
 from django.contrib.auth import login,logout,authenticate #用户验证相关
 from django.contrib.auth.decorators import login_required
@@ -34,7 +34,7 @@ def acc_login(request):
                             password=request.POST.get('password'))
         if user is not None:
             login(request,user)#登录用户
-            return HttpResponseRedirect('/bbs')
+            return HttpResponseRedirect(request.GET.get('next') or '/bbs')#登录后，返回到提交前的页面
         else:
             login_err = 'Wrong username or password!'
             return render(request,'bbs/html/login.html',{'login_err':login_err})
@@ -44,3 +44,23 @@ def acc_login(request):
 def acc_logout(request):
     logout(request)
     return  HttpResponseRedirect('/bbs')
+
+def article_detail(request,article_id):
+    article_obj = models.Article.objects.get(id=article_id)
+
+    return render(request,'bbs/html/article_detail.html',{'article_obj':article_obj,
+                                                          'category_list': category_list,})
+
+def comment(request):
+    print(request.POST)
+    if request.method =='POST':
+        new_comment_obj=models.Comment(
+            article_id= request.POST.get('article_id'),
+            parent_comment_id = request.POST.get('parent_comment_id') or None,
+            comment_type = request.POST.get('comment_type'),
+            user_id = request.user.userprofile.id,
+            comment = request.POST.get('comment')
+        )
+        new_comment_obj.save()
+
+        return HttpResponse('post-comment-success')
